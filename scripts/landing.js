15e676d9f2dfb13b42f2f4623dc263695bf4255c
Victoria Fung <victoriafung@Victorias-MBP.att.net>
2016-09-21 21:30:40 -0700

    Checkpoint 17 jQuery: Collection & Album View Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -10,13 +10,14 @@ var animatePoints = function(){
 
 
 $(window).load(function(){
-	if ($(window).height > 950){
+	if ($(window).height() > 950){
 		animatePoints();
 		}
 	var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 	
 	$(window).scroll(function(event) {
 		if($(window).scrollTop() >= scrollDistance){	
+			animatePoints();
 		}
 		});
 });
